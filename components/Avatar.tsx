fc22f4a1b6894232763588cc709480462214f6f8
Akash <aakasgdev24@gmail.com>
2023-06-28 12:48:39 +0530

    profile-image-fixed

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,53 +1,56 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
 
+import useUser from "@/hooks/useUser";
 
-interface AvatarProps{
-    userIds:string,
-    isLarge?:boolean 
-    hasBorder?:boolean
+interface AvatarProps {
+  userIds: string;
+  isLarge?: boolean;
+  hasBorder?: boolean;
 }
 
-import useUser from '@/hooks/useUser';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import React, { useCallback } from 'react'
+const Avatar: React.FC<AvatarProps> = ({ userIds, isLarge, hasBorder }) => {
+  const router = useRouter();
 
-const Avatar:React.FC<AvatarProps> = ({
-    userIds,
-    isLarge,
-    hasBorder
-}) => {
+  const { data: fetchedUser } = useUser(userIds);
 
-  const router=useRouter();
-    const {data:fetchUser}=useUser(userIds);
-const onClick=useCallback((event:any)=>{
-  event.stopPropagation();
+  const onClick = useCallback(
+    (event: any) => {
+      event.stopPropagation();
 
-  const url= `/user/${userIds}`
+      const url = `/user/${userIds}`;
 
-  router.push(url);
-},[router,userIds])
-
+      router.push(url);
+    },
+    [router, userIds]
+  );
+console.log("prof",fetchedUser)
   return (
     <div
       className={`
-    ${hasBorder ? "border-4 border-black" : ""}
-
-${isLarge ? "h-32" : "h-12"}
-${isLarge ? "w-32" : "w-12"}
-
-rounded-full
-hover:opacity-90
-transition
-cursor-pointer
-relative
-overflow-hidden
-`}
+        ${hasBorder ? "border-4 border-black" : ""}
+        ${isLarge ? "h-32" : "h-12"}
+        ${isLarge ? "w-32" : "w-12"}
+        rounded-full 
+        hover:opacity-90 
+        transition 
+        cursor-pointer
+        relative
+      `}
     >
-
-<Image onClick={onClick} src={fetchUser?.user?.profileImage|| '/images/placeholder.png'} alt="avatar" style={{objectFit:'cover',borderRadius:"100%"}} />
-
+      <Image
+        fill
+        style={{
+          objectFit: "cover",
+          borderRadius: "100%",
+        }}
+        alt="Avatar"
+        onClick={onClick}
+        src={fetchedUser?.user?.profileImage || "/images/placeholder.png"}
+      />
     </div>
   );
-}
+};
 
-export default Avatar
+export default Avatar;
